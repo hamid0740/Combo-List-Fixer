0966701a21c8dd705deebe75f228ecd2ad32749e
Hamidreza Dehghan <hamidd0740@gmail.com>
2021-10-05 20:06:06 +0330

    v1.2

--- Combo-Fixer.py
+++ Combo-Fixer.py
@@ -7,13 +7,14 @@ class color:
 	reset = '\033[0m'
 
 print(color.purple + "App name: " + color.reset + "Combo Fixer")
-print(color.purple + "Version: " + color.reset + "1.1")
+print(color.purple + "Version: " + color.reset + "1.2")
 print(color.purple + "Developer: " + color.reset + "hamid0740")
 print(color.purple + "GitHub: " + color.reset + "https://github.com/hamid0740/Combo-Fixer" + "\n")
 
-print(color.blue + "Make sure that this Python code is in the same directory with the combo you want to fix.")
+print(color.blue + "Make sure that this Python code is in the same directory with the combo list you want to fix.")
 file_name = input(color.yellow + "Enter your file name: " + color.reset)
-separator = input(color.yellow + "Enter your desired separator: " + color.reset + "\n")
+separator = input(color.yellow + "Enter your desired separator: " + color.reset)
+print("")
 
 file = open(file_name, "r")
 line_count = 0
@@ -25,15 +26,20 @@ file.close()
 with open(file_name, 'r') as file:
 	data = file.readlines()
 
-i = 1
+line_fixed = 0
 for i in range(line_count):
 	head, sep, tail = data[i].partition(separator)
-	if i+1 != line_count:
-		data[i] = head + "\n"
-	else:
-		data[i] = head
+	if separator in data[i]:
+		if i+1 != line_count:
+			data[i] = head + "\n"
+		else:
+			data[i] = head
+		line_fixed += 1
 
-with open(file_name, 'w') as file:
+with open("output.txt", 'w') as file:
 	file.writelines(data)
-	
-print(color.green + "Successfully edited " + str(line_count) + " lines in the " + file_name + " file." + color.reset)
+
+if line_fixed != 0:
+	print(color.green + "Successfully edited " + str(line_fixed) + " out of " + str(line_count) + " lines in the '" + file_name + "'' file.\nFixed file has been saved as 'output.txt'." + color.reset)
+else:
+	print(color.red + "No lines has been edited. Try changing the separator or note that your combo list may not have any problems." + color.reset)
