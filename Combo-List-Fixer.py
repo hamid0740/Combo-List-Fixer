7baa7729ad905c42f8d73ee189b0be606a62c17d
Hamidreza Dehghan <hamidd0740@gmail.com>
2021-10-09 22:36:00 +0330

    v1.3 (Removing duplicate combos)

--- Combo-List-Fixer.py
+++ Combo-List-Fixer.py
@@ -1,3 +1,4 @@
+#Color for texts
 class color:
 	purple = '\033[35m'
 	red = '\033[31m'
@@ -6,26 +7,24 @@ class color:
 	blue = '\033[36m'
 	reset = '\033[0m'
 
+#App info
 print(color.purple + "App name: " + color.reset + "Combo List Fixer")
-print(color.purple + "Version: " + color.reset + "1.2")
+print(color.purple + "Version: " + color.reset + "1.3")
 print(color.purple + "Developer: " + color.reset + "hamid0740")
 print(color.purple + "GitHub: " + color.reset + "https://github.com/hamid0740/Combo-List-Fixer" + "\n")
 
+#First inputs
 print(color.blue + "Make sure that this Python code is in the same directory with the combo list you want to fix.")
 file_name = input(color.yellow + "Enter your file name: " + color.reset)
 separator = input(color.yellow + "Enter your desired separator: " + color.reset)
 print("")
 
-file = open(file_name, "r")
-line_count = 0
-for line in file:
-	if line != "\n":
-		line_count += 1
-file.close()
-
+#Reading file and counting lines
 with open(file_name, 'r') as file:
 	data = file.readlines()
+	line_count = len(data)
 
+#Fixing lines by separating
 line_fixed = 0
 for i in range(line_count):
 	head, sep, tail = data[i].partition(separator)
@@ -36,9 +35,29 @@ for i in range(line_count):
 			data[i] = head
 		line_fixed += 1
 
+#Applying edits to file and printing the result
 if line_fixed != 0:
 	with open("output.txt", 'w') as file:
 		file.writelines(data)
-	print(color.green + "Successfully edited " + str(line_fixed) + " out of " + str(line_count) + " lines in the '" + file_name + "' file.\nFixed file has been saved as '" + color.red + "output.txt" + color.green + "'." + color.reset)
+	print(color.green + "Successfully edited " + str(line_fixed) + " out of " + str(line_count) + " lines in the '" + file_name + "' file.\nFixed file has been saved as '" + color.red + "output.txt" + color.green + "'." + "\n")
 else:
-	print(color.red + "No lines has been edited.\n Try changing the separator or note that your combo list may not have any problems." + color.reset)
+	print(color.red + "No line has been edited.\nTry changing the separator or note that your combo list may not have any problems." + "\n")
+
+#Asking to remove duplicate lines
+to_rem_dup = input(color.yellow + "Do you also want to remove duplicate combos? (y/n): " + color.reset)
+
+#Removing duplicate lines
+if to_rem_dup.lowercase().startswith("y"):
+	temp_data = []
+	for i in data:
+		if i not in temp_data:
+			temp_data.append(i)
+
+#Applying edits to file and printing the result
+	if len(temp_data) < len(data):
+		with open("output.txt", 'w') as file:
+			file.writelines(temp_data)
+		dup_lines = len(data) - len(temp_data)
+		print(color.green + "Successfully removed " + str(dup_lines) + " duplicate combos.\nFixed file has been saved as '" + color.red + "output.txt" + color.green + "'." + color.reset)
+	else:
+		print(color.red + "There were no duplicate combos found." + color.reset)
